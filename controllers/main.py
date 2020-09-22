00a4850abbad09e68645c15eff04c7a869259fc7
AngieMatahari <angie.mathare@gmail.com>
2020-09-22 12:37:41 +0300

    Cleanup post check

--- controllers/main.py
+++ controllers/main.py
@@ -16,9 +16,8 @@ class PosMpesaController(http.Controller):
     def mpesa_return(self, **post):
 
         checkout_request_id = post.get('CheckoutRequestID', None)
-        merchant_request_id = post.get('MerchantRequestID', None)
         _logger.info('notification received from mpesa:\n%s', pprint.pformat(post))
-        if checkout_request_id and merchant_request_id:
+        if checkout_request_id:
             pos_mpesa_payment = request.env['pos.mpesa.payment'].sudo().search([('checkout_request_id', '=', checkout_request_id)], limit=1)
             pos_mpesa_payment.sudo().feedback(post)
         
